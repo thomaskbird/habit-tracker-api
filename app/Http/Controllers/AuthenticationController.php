345ce09b5db3ea85fa6bf683886c080733384711
Tom Bird <thomas.bird1984@gmail.com>
2020-08-10 18:41:02 -0400

    Adding initial files

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,38 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use http\Client\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticationController extends Controller {
+    public function login_request(Request $request) {
+        $input = $request->except('_token');
+        print_r($input);
+
+        $validated = $request->validate([
+            'email' => 'required|unique:users',
+            'password' => 'required'
+        ]);
+
+        if($validated->fails()) {
+            return response(json_encode([
+                'status' => 'error',
+                'errors' => $validated->errors()
+            ]));
+        } else {
+            if(Auth::attempt($input)) {
+                return response(json_encode([
+                    'status' => 'success',
+                    'payload' => [
+                        'user' => Auth::user()
+                    ]
+                ]));
+            } else {
+                return response(json_encode([
+                    'status' => 'error',
+                    'errors' => ['The email or password did not match records, please try again!']
+                ]));
+            }
+        }
+    }
+}
